55a91fdeb6b996ce1ddc37f6350c31ba67be90f3
Jacob Cavazos-England <jacob.e.england@gmail.com>
2024-05-08 16:39:05 -0400

    feat: add support for custom colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,6 @@
 import { useEffect, useState } from "react";
 import Light from "./components/Light";
 
-
-interface ColorMap {
-  [key: string]: string; 
-}
-
-const colorMap: ColorMap = {
-  red: "rgb(255, 0, 0)",
-  yellow: "rgb(255, 255, 0)",
-  green: "rgb(0, 128, 0)",
-};
-
 interface LightsState {
     [colorName: string]: LightData;
 }
@@ -49,20 +38,25 @@ const lightOptions: LightOptions = {
 }; 
 
 
-
 function App() {
+  const [activeMode, setActiveMode] = useState("standard"); 
+  const [dropdownOptions, setDropdownOptions] = useState<string[]>([]);
   const [lightsData, setLightsData] = useState<LightsState>({})
   const [sequenceData, setSequenceData] = useState<LightSequenceItem[]>([])
   const [activeColors, setActiveColors] = useState<string[]>([]); 
   const [currentStepIndex, setCurrentStepIndex] = useState<number>(0); 
   
+  useEffect(() => {
+    setDropdownOptions(Object.keys(lightOptions)); 
+}, []);
 
   useEffect(() => {
-    const option = lightOptions.emergency
-
-    setLightsData(option.lights)
-    setSequenceData(option.sequence)
-  }, [])
+    const option = lightOptions[activeMode];
+    if (option) {
+        setLightsData(option.lights);
+        setSequenceData(option.sequence);
+    }  
+  }, [activeMode]); 
 
   useEffect(() => {
     if (sequenceData.length === 0) return;
@@ -81,24 +75,33 @@ function App() {
     }, nextChangeInterval);
 
     return () => clearTimeout(timeoutId); 
-}, [currentStepIndex, sequenceData]);
-
+  }, [currentStepIndex, sequenceData]);
 
   return (
-    <div style={{
-      width: 150,
-      height: 390,
-      backgroundColor: "black",
-      display: "flex",
-      flexDirection: "column",
-      justifyContent: "center",
-      alignItems: "center",
-      borderRadius: "10px"
-    }}>
-      {Object.entries(lightsData).map(([key, value]) => (
-        <Light key={key} color={colorMap[value.color]} text={key} toggled={activeColors.includes(value.color)} /> 
-    ))}
-    </div>
+    <>
+      <select value={activeMode} onChange={(e) => setActiveMode(e.target.value)}>
+            {dropdownOptions.map((option) => ( 
+                <option key={option} value={option}>{option}</option>
+            ))}
+      </select>
+      <div style={{
+        width: 150,
+        height: Object.entries(lightsData).length * 100 + 40,
+        backgroundColor: "black",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: "10px"
+      }}>
+        {Object.entries(lightsData).map(([key, value]) =>
+        {
+          return (
+            <Light key={key} color={value.color} text={key} toggled={activeColors.includes(key)} /> 
+          )
+        })}
+      </div>
+    </>
   )
 }
 
